802d67e74bff3345ef12ca46353039acb2327040
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2025-01-06 20:42:39 +0530

    Setup-configuration Update

--- 1.Basics/Lec1/io.cpp
+++ 1.Basics/Lec1/io.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main(){
-     
-    
+      int a;
+      cin >> a;
+      cout << a << endl;
+      
+      
+
+
    return 0;
 }
---
+++ jspwTest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+      vector<int> array;
+      cin >> array[0] >> array[1] >> array[2];
+      
+      cout << array[0] << endl;
+      cout << array[1] << endl;
+      cout << array[2] << endl;
+      
+      
+
+
+   return 0;
+}
